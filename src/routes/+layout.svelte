4607470333326e186456d2b0c8d50d44ab5a5243
i.kłusek <J6ZkqwVUn>
2023-11-06 14:44:07 +0100

    AC-01-Project init structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import 'modern-normalize/modern-normalize.css';
+	import '../styles/main.scss';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
+<div class="wrapper">
+	<slot />
+</div>
+
+<style lang="scss">
+	.wrapper {
+		background-color: var(--color-neutral-white);
+		border-radius: 15px;
+		border-bottom-right-radius: 25%;
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 10px;
+		width: fit-content;
+		padding: 15px;
+		margin: 10px;
+	}
+</style>
